14eeafca5e981b6652bf80a547a960553e1e6771
Alex Ermolov <aaermolov@gmail.com>
2021-05-09 16:28:00 +0300

    impl: ui: added desktop notifications

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/wiedzmin/toolbox
 go 1.16
 
 require (
+	github.com/0xAX/notificator v0.0.0-20191016112426-3962a5ea8da1
 	github.com/Jeffail/gabs v1.4.0
 	github.com/mediocregopher/radix/v3 v3.7.0
 	github.com/urfave/cli/v2 v2.3.0
--- impl/ui/ui.go
+++ impl/ui/ui.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/0xAX/notificator"
 	"github.com/wiedzmin/toolbox/impl/shell"
 )
 
@@ -11,9 +12,26 @@ const (
 	rofiOptionsSeparator = "@"
 )
 
+var notify *notificator.Notificator
+
+func init() {
+	notify = notificator.New(notificator.Options{
+		// DefaultIcon: "icon/default.png",
+		AppName: "webjumps",
+	})
+}
+
 // GetSelectionRofi returns users choice from list of options, using Rofi selector tool
 func GetSelectionRofi(seq []string, prompt string) (string, error) {
 	seqStr := strings.Join(seq, rofiOptionsSeparator)
 	result, err := shell.ShellCmd(fmt.Sprintf("rofi -dmenu -sep %s -p '%s'", rofiOptionsSeparator, prompt), &seqStr, nil, true)
 	return *result, err
+}
+
+func NotifyNormal(title, text string) {
+	notify.Push(title, text, "", notificator.UR_NORMAL)
+}
+
+func NotifyCritical(title, text string) {
+	notify.Push(title, text, "", notificator.UR_CRITICAL)
 }
