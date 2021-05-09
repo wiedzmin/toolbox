24cff09a0021152324325843e8c73b9603c3176d
Alex Ermolov <aaermolov@gmail.com>
2021-05-09 16:30:10 +0300

    impl: env: added Redis values setting method

--- impl/env/env.go
+++ impl/env/env.go
@@ -17,3 +17,18 @@ func GetRedisValue(key string, client *radix.Pool) ([]byte, *radix.Pool, error) 
 	}
 	return result, client, nil
 }
+
+func SetRedisValue(key, value string, client *radix.Pool) (*radix.Pool, error) {
+	var err error
+	if client == nil {
+		client, err = radix.NewPool("tcp", "127.0.0.1:6379", 1)
+		if err != nil {
+			return nil, err
+		}
+	}
+	err = client.Do(radix.Cmd(nil, "SET", key, value))
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
