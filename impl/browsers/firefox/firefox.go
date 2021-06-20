70416bf4573b0f28c09ba82a784a9a01edf1abee
Alex Ermolov <aaermolov@gmail.com>
2021-06-20 23:07:39 +0300

    browsers: firefox: added logging

--- impl/browsers/firefox/firefox.go
+++ impl/browsers/firefox/firefox.go
@@ -4,6 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"os/user"
+
+	"github.com/wiedzmin/toolbox/impl"
+	"go.uber.org/zap"
 )
 
 type SessionFormat int8
@@ -13,8 +16,16 @@ const (
 	SESSION_FORMAT_ORG  SessionFormat = 1
 )
 
+var logger *zap.Logger
+
+func init() {
+	logger = impl.NewLogger()
+}
+
 func RawSessionsPath() (*string, error) {
+	l := logger.Sugar()
 	userInfo, err := user.Current()
+	l.Debugw("[RawSessionsPath]", "userInfo", userInfo)
 	if err != nil {
 		return nil, err
 	}
@@ -22,5 +33,6 @@ func RawSessionsPath() (*string, error) {
 		return nil, errors.New("current user has no home directory")
 	}
 	result := fmt.Sprintf("%s/.mozilla/firefox/profile.default/sessionstore-backups", userInfo.HomeDir)
+	l.Debugw("[RawSessionsPath]", "result", result)
 	return &result, nil
 }
