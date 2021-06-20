f841120014a90215e06c4ced99aa12561248a5bf
Alex Ermolov <aaermolov@gmail.com>
2021-06-21 00:21:41 +0300

    emacs: added logging

--- impl/emacs/emacs.go
+++ impl/emacs/emacs.go
@@ -4,19 +4,32 @@ import (
 	"fmt"
 	"os/user"
 
+	"github.com/wiedzmin/toolbox/impl"
 	"github.com/wiedzmin/toolbox/impl/shell"
+	"go.uber.org/zap"
 )
 
+var logger *zap.Logger
+
+func init() {
+	logger = impl.NewLogger()
+}
+
 func SocketPath() (*string, error) {
+	l := logger.Sugar()
 	userInfo, err := user.Current()
+	l.Debugw("[SocketPath]", "userInfo", userInfo, "err", err)
 	if err != nil {
 		return nil, err
 	}
 	result := fmt.Sprintf("/run/user/%s/emacs/server", userInfo.Uid)
+	l.Debugw("[SocketPath]", "socket path", result)
 	return &result, nil
 }
 
 func SendToServer(elisp string) error {
+	l := logger.Sugar()
+	l.Debugw("[SendToServer]", "elisp", elisp)
 	socketPath, err := SocketPath()
 	if err != nil {
 		return err
