58d4f82e843ccd99f2115b51776851618f837855
Alex Ermolov <aaermolov@gmail.com>
2021-06-21 11:37:16 +0300

    tmux: added logging

--- impl/tmux/tmux.go
+++ impl/tmux/tmux.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/wiedzmin/toolbox/impl"
 	"github.com/wiedzmin/toolbox/impl/shell"
+	"go.uber.org/zap"
 )
 
 type Session struct {
@@ -19,8 +21,17 @@ func (e ErrSessionNotFound) Error() string {
 	return fmt.Sprintf("tmuxp: session '%s' not exist", e.Name)
 }
 
+var logger *zap.Logger
+
+func init() {
+	logger = impl.NewLogger()
+}
+
 func GetSession(name string, create, attach bool) (*Session, error) {
+	l := logger.Sugar()
+	l.Debugw("[GetSession]", "name", name, "create", create, "attach", attach)
 	out, err := shell.ShellCmd(fmt.Sprintf("tmux has-session -t %s", name), nil, nil, true, false)
+	l.Debugw("[GetSession]", "out", out, "err", err)
 	if err != nil {
 		return nil, err
 	}
@@ -39,10 +50,12 @@ func GetSession(name string, create, attach bool) (*Session, error) {
 }
 
 func (s *Session) NewWindow(cmd, title, startDirectory string, attach bool) error {
+	l := logger.Sugar()
 	args := []string{
 		fmt.Sprintf("-t %s", s.Name),
 		fmt.Sprintf("-n %s", title),
 	}
+	l.Debugw(fmt.Sprintf("[%s.NewWindow]", s.Name), "cmd", cmd, "title", title, "startDirectory", startDirectory, "attach", attach)
 	var argsStr strings.Builder
 	if !attach {
 		args = append(args, "-d")
@@ -56,6 +69,7 @@ func (s *Session) NewWindow(cmd, title, startDirectory string, attach bool) erro
 	}
 
 	tmuxCmd := fmt.Sprintf("tmux new-window %s", strings.TrimSpace(argsStr.String()))
+	l.Debugw(fmt.Sprintf("[%s.NewWindow]", s.Name), "tmuxCmd", tmuxCmd)
 	_, err := shell.ShellCmd(tmuxCmd, nil, nil, false, false)
 	if err != nil {
 		return err
