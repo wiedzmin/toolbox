c9cde129dfa6008bfe7c00bbdda2ae2070aa6fbe
Alex Ermolov <aaermolov@gmail.com>
2021-06-21 15:20:58 +0300

    xserver: reimplemented `GetCurrentWindowName` using xgbutil

--- impl/xserver/xserver.go
+++ impl/xserver/xserver.go
@@ -1,37 +1,22 @@
 package xserver
 
 import (
-	"github.com/jezek/xgb"
-	"github.com/jezek/xgb/xproto"
+	"github.com/jezek/xgbutil"
+	"github.com/jezek/xgbutil/ewmh"
 )
 
-func GetCurrentWindowName(X *xgb.Conn) (*string, error) {
+func GetCurrentWindowName(X *xgbutil.XUtil) (*string, error) {
 	var err error
 	if X == nil {
-		X, err = xgb.NewConn()
+		X, err = xgbutil.NewConn()
 		if err != nil {
 			return nil, err
 		}
 	}
-	setup := xproto.Setup(X)
-	root := setup.DefaultScreen(X).Root
-	activeAtom, err := xproto.InternAtom(X, true, uint16(len("_NET_ACTIVE_WINDOW")), "_NET_ACTIVE_WINDOW").Reply()
+	active, err := ewmh.ActiveWindowGet(X)
 	if err != nil {
 		return nil, err
 	}
-	nameAtom, err := xproto.InternAtom(X, true, uint16(len("_NET_WM_NAME")), "_NET_WM_NAME").Reply()
-	if err != nil {
-		return nil, err
-	}
-	reply, err := xproto.GetProperty(X, false, root, activeAtom.Atom, xproto.GetPropertyTypeAny, 0, (1<<32)-1).Reply()
-	if err != nil {
-		return nil, err
-	}
-	windowId := xproto.Window(xgb.Get32(reply.Value))
-	reply, err = xproto.GetProperty(X, false, windowId, nameAtom.Atom, xproto.GetPropertyTypeAny, 0, (1<<32)-1).Reply()
-	if err != nil {
-		return nil, err
-	}
-	windowName := string(reply.Value)
-	return &windowName, nil
+	windowName, err := ewmh.WmNameGet(X, active)
+	return &windowName, err
 }
