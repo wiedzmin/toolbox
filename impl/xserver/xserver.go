90e57d169d7f25329fac6cf991b244921c0bd66d
Alex Ermolov <aaermolov@gmail.com>
2021-06-21 17:22:41 +0300

    xserver: added window queries functionality

--- impl/xserver/xserver.go
+++ impl/xserver/xserver.go
@@ -1,9 +1,104 @@
 package xserver
 
 import (
+	"fmt"
+	"regexp"
+
+	"github.com/jezek/xgb/xproto"
 	"github.com/jezek/xgbutil"
 	"github.com/jezek/xgbutil/ewmh"
+	"github.com/jezek/xgbutil/icccm"
 )
+
+type WindowQuery struct {
+	Name           string
+	nameRegexp     *regexp.Regexp
+	Class          string
+	classRegexp    *regexp.Regexp
+	Instance       string
+	instanceRegexp *regexp.Regexp
+	Fuzzy          bool
+}
+
+type ErrWindowNotFound struct {
+	Query WindowQuery
+}
+
+func (e ErrWindowNotFound) Error() string {
+	return fmt.Sprintf("no windows found by query: %v", e.Query)
+}
+
+func (q WindowQuery) Empty() bool {
+	return q.Name == "" && q.Class == "" && q.Instance == ""
+}
+
+func (q WindowQuery) MatchWindow(X *xgbutil.XUtil, win xproto.Window) bool {
+	if q.Empty() {
+		return false
+	}
+	var wmClassData *icccm.WmClass
+	var err error
+	if q.Class != "" || q.Instance != "" {
+		wmClassData, err = icccm.WmClassGet(X, win)
+		if err != nil {
+			return false
+		}
+	}
+	match := true
+	if q.Name != "" {
+		if q.Fuzzy {
+			if q.nameRegexp == nil {
+				return false
+			} else {
+				windowName, err := ewmh.WmNameGet(X, win)
+				if err != nil {
+					return false
+				}
+				if !q.nameRegexp.MatchString(windowName) {
+					return false
+				}
+			}
+		}
+	}
+	if q.Class != "" {
+		if q.Fuzzy {
+			if q.classRegexp == nil {
+				return false
+			} else {
+				if !q.classRegexp.MatchString(wmClassData.Class) {
+					return false
+				}
+			}
+		}
+	}
+	if q.Instance != "" {
+		if q.Fuzzy {
+			if q.instanceRegexp == nil {
+				return false
+			} else {
+				if !q.instanceRegexp.MatchString(wmClassData.Instance) {
+					return false
+				}
+			}
+		}
+	}
+	return match
+}
+
+func prepareWindowQuery(query WindowQuery) WindowQuery {
+	if query.Fuzzy {
+		if query.Name != "" {
+			query.nameRegexp = regexp.MustCompile(query.Name)
+		}
+		if query.Class != "" {
+			query.classRegexp = regexp.MustCompile(query.Class)
+		}
+		if query.Instance != "" {
+			query.instanceRegexp = regexp.MustCompile(query.Instance)
+		}
+	}
+	return query
+}
 
 func GetCurrentWindowName(X *xgbutil.XUtil) (*string, error) {
 	var err error
