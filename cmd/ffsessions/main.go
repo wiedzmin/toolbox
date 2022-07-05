1d4a06d01b07dd9c697de2921a0b53fd0bf33aea
Alex Ermolov <aaermolov@gmail.com>
2022-07-05 23:17:58 +0300

    cmd: added Firefox sessions dumping

---
+++ cmd/ffsessions/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+	"github.com/wiedzmin/toolbox/impl"
+	"github.com/wiedzmin/toolbox/impl/browsers/firefox"
+	"github.com/wiedzmin/toolbox/impl/ui"
+)
+
+func dump(ctx *cli.Context) error {
+	sessionsPath := firefox.RawSessionsPath()
+	if sessionsPath == nil {
+		return fmt.Errorf("error getting firefox sessions path root")
+	}
+	session, err := firefox.LoadSession(fmt.Sprintf("%s/recovery.jsonlz4", *sessionsPath))
+	if err != nil {
+		return err
+	}
+
+	var sessionFormat firefox.SessionFormat
+	var sessionExtension string
+	switch {
+	case ctx.Bool("json"):
+		sessionFormat = firefox.SESSION_FORMAT_JSON
+		sessionExtension = "json"
+	case ctx.Bool("flat"):
+		sessionFormat = firefox.SESSION_FORMAT_ORG_FLAT
+		sessionExtension = "org"
+	default:
+		sessionFormat = firefox.SESSION_FORMAT_ORG
+		sessionExtension = "org"
+	}
+
+	var sessionName string
+	if ctx.String("out") != "" {
+		sessionName = ctx.String("out")
+	} else {
+		sessionName = fmt.Sprintf("%s-%s.%s", ctx.String("dump-basename"), impl.CommonNowTimestamp(), sessionExtension)
+	}
+
+	return firefox.DumpSession(
+		fmt.Sprintf("%s/%s", ctx.String("dumps-path"), sessionName),
+		session,
+		sessionFormat,
+		ctx.Bool("raw"),
+		ctx.Bool("keep-tabs-history"),
+	)
+}
+
+func createCLI() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Ffsessions"
+	app.Usage = "Firefox sessions management tool"
+	app.Description = "Ffsessions"
+	app.Version = "0.0.1#master"
+
+	app.Commands = cli.Commands{
+		{
+			Name:   "dump",
+			Usage:  "Dump session",
+			Action: dump,
+			Flags: []cli.Flag{
+				&cli.BoolFlag{
+					Name:     "raw",
+					Usage:    "Dump raw URLs without descriptions/titles",
+					Required: false,
+				},
+				&cli.BoolFlag{
+					Name:     "json",
+					Aliases:  []string{"j"},
+					Usage:    "Dump session in JSON format",
+					Required: false,
+				},
+				&cli.BoolFlag{
+					Name:     "flat",
+					Usage:    "Dump flat Org session layout, without windows breakdown",
+					Required: false,
+				},
+				&cli.StringFlag{
+					Name:     "out",
+					Aliases:  []string{"o"},
+					Usage:    "Dump filename",
+					Required: false,
+				},
+				&cli.StringFlag{
+					Name:  "dump-basename",
+					Usage: "Dump basename",
+					Value: "firefox-session-auto",
+				},
+				&cli.BoolFlag{
+					Name:     "keep-tabs-history",
+					Aliases:  []string{"k"},
+					Value:    false,
+					Usage:    "Also dump links from tab history",
+					Required: false,
+				},
+			},
+		},
+	}
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:     "dumps-path",
+			Usage:    "Path to store dumps under",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     impl.SelectorFontFlagName,
+			Aliases:  []string{"f"},
+			EnvVars:  []string{impl.EnvPrefix + "_SELECTOR_FONT"},
+			Usage:    "Font to use for selector application, e.g. dmenu, rofi, etc.",
+			Required: false,
+		},
+		&cli.StringFlag{
+			Name:     ui.SelectorToolFlagName,
+			Aliases:  []string{"T"},
+			EnvVars:  []string{impl.EnvPrefix + "_SELECTOR_TOOL"},
+			Value:    ui.SelectorTool,
+			Usage:    "Selector tool to use, e.g. dmenu, rofi, etc.",
+			Required: false,
+		},
+	}
+	return app
+}
+
+func main() {
+	app := createCLI()
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
