25e1fa81994bd7dc4cadc26f5281055025daeb16
Alex Ermolov <aaermolov@gmail.com>
2025-06-22 16:21:42 +0300

    qbcli: added interactive quiering

--- cmd/qbcli/main.go
+++ cmd/qbcli/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/wiedzmin/toolbox/impl"
 	"github.com/wiedzmin/toolbox/impl/browsers/qutebrowser"
 	"github.com/wiedzmin/toolbox/impl/redis"
+	"github.com/wiedzmin/toolbox/impl/ui"
+	"github.com/wiedzmin/toolbox/impl/xserver/xkb"
 	"go.uber.org/zap"
 )
 
@@ -30,6 +32,15 @@ func getCurrentTarget() (string, error) {
 	return target, nil
 }
 
+func query(ctx *cli.Context, prompt string) (string, error) {
+	xkb.EnsureEnglishKeyboardLayout()
+	value, err := ui.GetSelection([]string{}, prompt, ctx.String(ui.SelectorToolFlagName), ctx.String(impl.SelectorFontFlagName), true, false)
+	if err != nil {
+		return "", err
+	}
+	return value, nil
+}
+
 func open(ctx *cli.Context) error {
 	var target, openParam string
 
@@ -47,12 +58,29 @@ func open(ctx *cli.Context) error {
 		return fmt.Errorf("unknown url target '%s'", target)
 	}
 
+	var url string
+	if ctx.Bool("query") {
+		url, err = query(ctx, "URL")
+		if err != nil {
+			return err
+		}
+	} else {
+		url = ctx.String("url")
+	}
+
 	return qutebrowser.Execute([]string{
-		fmt.Sprintf(":open %s %s", openParam, ctx.String("url")),
+		fmt.Sprintf(":open %s %s", openParam, url),
 	})
 }
 
 func saveSession(ctx *cli.Context) error {
+	if ctx.Bool("query") {
+		sessionName, err := query(ctx, "save as")
+		if err != nil {
+			return err
+		}
+		return qutebrowser.SaveSessionInternal(sessionName)
+	}
 	return qutebrowser.SaveSessionInternal(ctx.String("name"))
 }
 
@@ -75,6 +103,11 @@ func createCLI() *cli.App {
 					Usage:    "URL to open",
 					Required: true,
 				},
+				&cli.BoolFlag{
+					Name:     "query",
+					Usage:    "Whether to show selector for argument input",
+					Required: false,
+				},
 			},
 		},
 		{
@@ -88,7 +121,29 @@ func createCLI() *cli.App {
 					Usage:    "Session name",
 					Required: false,
 				},
+				&cli.BoolFlag{
+					Name:     "query",
+					Usage:    "Whether to show selector for argument input",
+					Required: false,
+				},
 			},
+		},
+	}
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:     impl.SelectorFontFlagName,
+			Aliases:  []string{"f"},
+			EnvVars:  []string{impl.EnvPrefix + "_SELECTOR_FONT"},
+			Usage:    "Font to use for selector application, e.g. dmenu, rofi, etc.",
+			Required: false,
+		},
+		&cli.StringFlag{
+			Name:     ui.SelectorToolFlagName,
+			Aliases:  []string{"T"},
+			EnvVars:  []string{impl.EnvPrefix + "_SELECTOR_TOOL"},
+			Value:    ui.SelectorToolDefault,
+			Usage:    "Selector tool to use, e.g. dmenu, rofi, etc.",
+			Required: false,
 		},
 	}
 	return app
